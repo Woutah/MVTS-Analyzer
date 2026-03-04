dd3346b478674c397f054c08e078639317515b35
Wouter Stokman <Wouterstokman@hotmail.com>
2026-03-04 21:41:46 +0100

    Update version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name = "MVTS-Analyzer",
-	version= "0.0.2",
+	version= "0.0.3",
 	packages=find_packages('.'),
     description=("Plotting, annotation and analysis tools for multivariate time series data"),
     long_description=open('README.md', encoding='utf-8').read(),
